bd7ddad714cecdf113cde1a7ecc45a4f53b7d637
kamaseinu <48120534+kamaseinu@users.noreply.github.com>
2020-05-04 20:11:01 +0900

    Update CameraDirector.cs

--- CameraDirector.cs
+++ CameraDirector.cs
@@ -1,26 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraDirector : MonoBehaviour
 {
+  
     private GameObject player;   //プレイヤー情報格納用
-    private Vector3 offset;      //相対距離取得用
+    private Vector3 ppp = new Vector3(0, 10, 0);    //10のところを変えるとカメラの視野が変わる
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //unitychanの情報を取得
-        this.player = GameObject.Find("me");
 
-        // MainCamera(自分自身)とplayerとの相対距離を求める
-        offset = transform.position - player.transform.position;
-    }
-
-    // Update is called once per frame
     void Update()
     {
+
+        //プレイヤーの座標を取得
+        this.player = GameObject.Find("me(Clone)");
+
         //新しいトランスフォームの値を代入する
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + ppp;
     }
 }
